4df2eebf219ee3da188c36520470d9cec2dd9023
AbdullahAlFalah <salem908mk@gmail.com>
2024-07-21 14:20:01 +0300

    HoverHints & and Resume addition from google drive

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -3,18 +3,23 @@ import Menuitem from './Menuitem'
 import { IoIosHome, IoIosInformationCircle} from "react-icons/io";
 import { RiContactsFill } from "react-icons/ri";
 import { FaFile } from "react-icons/fa";
+import { MdAttachFile } from "react-icons/md";
 
 export default function Header() {
   return (
-    <div className='flex justify-between items-center text-white bg-green-700'>
+    <div className='flex justify-between items-center text-white bg-green-700 p-2'>
         <div className='flex gap-4 px-4'>
             <Menuitem title='Home' address='/' Icon={IoIosHome}/>
             <Menuitem title='About Me' address='/pages/AboutMe' Icon={IoIosInformationCircle}/>
-            <Menuitem title='Contacts' address='/pages/ContactMe' Icon={RiContactsFill}/>
+            <Menuitem title='Contact Me' address='/pages/ContactMe' Icon={RiContactsFill}/>
         </div>
         <div className='flex gap-4 px-4'>
             <Menuitem title='Portfolio' address='/pages/Portfolio' Icon={FaFile}/> 
-
+            <a href="https://drive.google.com/file/d/17hGDKqmFwgO7malpvNsVA_eAbtbLtKYG/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="hover:text-amber-400 flex items-center group relative">
+              <span className="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 text-sm text-gray-100 rounded-md absolute left-1/2 -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">Resume</span>
+              <MdAttachFile className='text-4xl sm:hidden'/>
+              <p className='uppercase hidden sm:inline text-base'>Resume</p>
+            </a>
         </div>
     </div>
   )
--- src/components/ui/Menuitem.tsx
+++ src/components/ui/Menuitem.tsx
@@ -10,11 +10,13 @@ type Menuitemdetails = {
 
 export default function Menuitem({title, address, Icon}: Menuitemdetails) {
   return (
-    <Link to={address}>
+    <Link to={address} className='group flex relative'>
         <div className='hover:text-amber-400'>
-            <Icon className='text-4xl sm:hidden'/>
-            <p className='uppercase hidden sm:inline text-base'>{title}</p>  
+          <span className="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 text-sm text-gray-100 rounded-md absolute left-1/2 -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">{title}</span>
+          <Icon className='text-4xl sm:hidden'/>
+          <p className='uppercase hidden sm:inline text-base'>{title}</p>  
         </div>
     </Link>
   )
 }
+

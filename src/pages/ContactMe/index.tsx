03f4884bbcada7dc80797db12f90b063ef2df21e
AbdullahAlFalah <salem908mk@gmail.com>
2025-08-07 19:54:02 +0300

    Additions to styling

--- src/pages/ContactMe/index.tsx
+++ src/pages/ContactMe/index.tsx
@@ -2,16 +2,20 @@ import './ContactMe.css';
 
 export default function ContactMe() {
   return (
-    <div className='contact-container'>
-      <h2 className='contact-title'>Contact Me:</h2>
-      <div className='contact-links'>
-        <a href="https://www.linkedin.com/in/abdullah-al-falah-5502a1192" target="_blank" rel="noopener noreferrer">
-          My LinkedIn Profile
-        </a>
-        <a href="https://github.com/AbdullahAlFalah" target="_blank" rel="noopener noreferrer">
-          My GitHub Profile
-        </a>
+    <div className='contact-container max-w-2xl mx-auto py-8 px-4'>
+      <h2 className='contact-title text-3xl sm:text-4xl font-bold mb-8'>Contact Me:</h2>
+
+      <div className="mb-10 flex flex-col items-center gap-3 bg-white rounded-lg p-5 sm:p-8 border-4 border-blue-400 hover:border-yellow-500 transition-all duration-300">
+        <div className='contact-links'>
+          <a href="https://www.linkedin.com/in/abdullah-al-falah-5502a1192" target="_blank" rel="noopener noreferrer">
+            My LinkedIn Profile
+          </a>
+          <a href="https://github.com/AbdullahAlFalah" target="_blank" rel="noopener noreferrer">
+            My GitHub Profile
+          </a>
+        </div>
       </div>
+
     </div>
   )
 }
--- src/pages/Portfolio/index.tsx
+++ src/pages/Portfolio/index.tsx
@@ -7,16 +7,47 @@ export default function Portfolio() {
   const mishmashUrl = 'https://drive.google.com/uc?export=download&id=';
 
   return (
-    <div className='portfolio-container'>
-      <h2 className='portfolio-title'>Portfolio:</h2>
-      <DownloadButton
-        url={sudokuUrl}
-        label="Download Suduko's APK"
-      />
-      <DownloadButton
-        url={mishmashUrl}
-        label="Download MishMash's APK"
-      />
+    <div className='portfolio-container max-w-2xl mx-auto py-8 px-4'>
+      <h2 className='portfolio-title text-3xl sm:text-4xl font-bold mb-8'>Portfolio:</h2>
+
+      <div className="mb-10 flex flex-col items-start gap-3 bg-white rounded-lg shadow p-5 sm:p-8">
+        <div className="text-lg sm:text-xl font-semibold">Sudoku App:</div>
+        <p className="text-base sm:text-lg mb-2 text-gray-700 text-left w-full">
+          This is a Sudoku game I built using vanilla React Native. It features 3 difficulty levels and a clean simple non-responsive UI.<br />
+          This app was built as a learning project to understand the basics of React Native and mobile app development.<br />
+          It includes features like a timer, a hint system to help players, a dynamic theme feature and a local notification system.<br />
+          This app is not connected to any backend, with a single exception that a simple API call made to fetch a random "Fact of the Day".<br />
+        </p>
+        <DownloadButton
+          url={sudokuUrl}
+          label="Download Suduko's APK"
+        />
+      </div>
+
+      <div className="mb-10 flex flex-col items-start gap-3 bg-white rounded-lg shadow p-5 sm:p-8">
+        <div className="text-lg sm:text-xl font-semibold">MishMash App:</div>
+        <p className="text-base sm:text-lg mb-2 text-gray-700 text-left w-full">
+          MishMash, as the name suggests, is a mish mash of various features and functionalities. It is an Expo React Native app that showcases my skills in building complex applications.<br />
+          This app includes complex navigation, state management, and a variety of responsive UI components.<br />
+          It's design is a bit chaotic, just like the whole process of building it, but it's connected to my own self-developed backend and has lots of the most requested features in the industry and all developed to its standards.<br />
+          This app has a remote notification system, login and registration system, ads implemenation, remote background image upgrade system, and a lot more.<br />   
+        </p>
+        <a 
+          className="link text-base sm:text-lg mb-2 text-gray-700 text-left w-full"
+          href='https://github.com/AbdullahAlFalah/my-node-express/tree/new-main' 
+          target="_blank" 
+          rel="noopener noreferrer"
+        >
+          View MishMash's Backend Code Here!
+        </a>
+        <DownloadButton
+          url={mishmashUrl}
+          label="Download MishMash's APK"
+        />
+      </div>
+
+
+
     </div>
   )
 
